9c3cb66d106393efef838c901e2742ba5e418c1e
Tomás Brás <tomasbras@ua.pt>
2024-05-02 11:16:46 +0100

    .

--- roomwave/src/components/ImageHomeesqueda.tsx
+++ roomwave/src/components/ImageHomeesqueda.tsx
@@ -11,8 +11,7 @@ const ImageHomeesquerda= () => {
                     <div className="content">
                         <h3>Já escolheste o estabelecimento de ensino?</h3>
                         <p>Escolher uma universidade é uma das decisões mais significativas que uma pessoa pode fazer em sua jornada educacional e profissional</p>
-                        <p>A importância desse processo vai além do simples ato de selecionar uma instituição de ensino superior; envolve uma série de considerações que moldarão não apenas os anos acadêmicos, mas também o futuro pessoal e profissional de um indivíduo. </p>
-                        <Searchuni/>
+                        <p>A importância desse processo vai além do simples ato de selecionar uma instituição de ensino superior; envolve uma série de considerações que moldarão não apenas os anos acadêmicos, mas também o futuro pessoal e profissional de um indivíduo. </p> 
                     </div>
                     <div className="video-container">
                         <video src="./src/images/universidade.mp4" loop autoPlay muted></video>
