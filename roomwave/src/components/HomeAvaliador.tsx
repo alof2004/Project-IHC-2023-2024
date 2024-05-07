a260f383d51d55a5421a3d0b2dd91908ce976e3e
Tomás Brás <tomasbras@ua.pt>
2024-05-07 16:28:10 +0100

    .

--- roomwave/src/components/HomeAvaliador.tsx
+++ roomwave/src/components/HomeAvaliador.tsx
@@ -1,20 +1,15 @@
-import React, { useState } from 'react';
 import '../css/HomeAvaliador.css';
 import NavBarAvaliador from './NavBarAvaliador';
-import { useUser } from "./UserContext";
-import { useNavigate } from 'react-router-dom';
-import NavBar from "./NavBar";
-import Footer from "./footer";
+
 
 
 function HomeAvaliador() {
     return (
-        <div>
+        <><div>
             <NavBarAvaliador />
-        </div>
-        <div>
-            <h1>HomeAvaliador</h1>
-        </div>
+        </div><div>
+                <h1>HomeAvaliador</h1>
+            </div></>
     );
 }
 
