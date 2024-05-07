2329b1b83da59acaffb756fc10ab456b02661499
João Neto <jneto04@ua.pt>
2024-05-07 16:21:36 +0100

    aaaaaaa

--- roomwave/src/components/HomeAvaliador.tsx
+++ roomwave/src/components/HomeAvaliador.tsx
@@ -1,5 +1,15 @@
+import React, { useState } from 'react';
+import '../css/HomeAvaliador.css';
+import NavBarAvaliador from './NavBarAvaliador';
+import { useUser } from "./UserContext";
+import { useNavigate } from 'react-router-dom';
+
+
 function HomeAvaliador() {
     return (
+        <div>
+            <NavBarAvaliador />
+        </div>
         <div>
             <h1>HomeAvaliador</h1>
         </div>
