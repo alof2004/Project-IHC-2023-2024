3f584e8acf932033ec583d7a0905bc6000a4e9ce
Tomás Brás <tomasbras@ua.pt>
2024-05-07 16:03:24 +0100

    .

---
+++ roomwave/src/components/Certificador.tsx
@@ -0,0 +1,14 @@
+import NavBarClient from "./NavBarClient";
+import Footer from "./footer";
+
+
+function Certificador() {
+    return (
+        <><div>
+            <NavBarClient />
+
+        </div><Footer /></>
+    );
+}
+
+export default Certificador;
