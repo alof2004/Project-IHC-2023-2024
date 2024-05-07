8bc31574683f60a8071afd479c356926bfc4787b
alof04 <hpclafonso@gmail.com>
2024-05-07 16:27:22 +0100

    .

--- roomwave/src/components/HomeAvaliador.tsx
+++ roomwave/src/components/HomeAvaliador.tsx
@@ -1,8 +1,3 @@
-import React, { useState } from 'react';
-import '../css/HomeAvaliador.css';
-import NavBarAvaliador from './NavBarAvaliador';
-import { useUser } from "./UserContext";
-import { useNavigate } from 'react-router-dom';
 import NavBar from "./NavBar";
 import NavBarAvaliador from "./NavBarAvaliador";
 import Footer from "./footer";
@@ -10,19 +5,10 @@ import Footer from "./footer";
 
 function HomeAvaliador() {
     return (
-<<<<<<< HEAD
         <><div>
             <NavBarAvaliador />
 
         </div><Footer /></>
-=======
-        <div>
-            <NavBarAvaliador />
-        </div>
-        <div>
-            <h1>HomeAvaliador</h1>
-        </div>
->>>>>>> refs/remotes/origin/main
     );
 }
 
