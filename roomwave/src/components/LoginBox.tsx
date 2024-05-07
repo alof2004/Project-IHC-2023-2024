1f48d2fd97bb688a010a8c71ad3806ba2c6d9fb3
alof04 <hpclafonso@gmail.com>
2024-05-07 15:32:09 +0100

    .

--- roomwave/src/components/LoginBox.tsx
+++ roomwave/src/components/LoginBox.tsx
@@ -29,7 +29,7 @@ const LoginBox: React.FC = () => {
   const redirectToSavedPath = () => {
     const savedPath = localStorage.getItem('redirectPath');
     console.log('Attempting to navigate to:', savedPath); // Debugging line
-    if (savedPath) {
+    if (savedPath && savedPath != "/Home" && savedPath != "/")  {
       navigate(savedPath);
       localStorage.removeItem('redirectPath');
       console.log('Navigated to:', savedPath); // Debugging line
