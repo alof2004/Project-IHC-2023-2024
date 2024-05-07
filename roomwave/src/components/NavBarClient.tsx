39932e014cbf76a97ef0ef3c58ce1639fc8105e5
alof04 <hpclafonso@gmail.com>
2024-05-07 16:13:38 +0100

    .

--- roomwave/src/components/NavBarClient.tsx
+++ roomwave/src/components/NavBarClient.tsx
@@ -17,6 +17,7 @@ function NavBarClient() {
     setTimeout(() => {
       setShowLogoutAlert(false); // Fecha o alerta após 3 
       localStorage.removeItem("userData"); // Remove o item "userData" do localStorage
+      localStorage.removeItem("visitedRooms"); // Remove o item "visitedRooms" do localStorage
       navigate('/login'); // Redireciona para a página de login após 
     }, 1000); // Tempo em milissegundos para manter o alerta visível antes de fechar
   };
