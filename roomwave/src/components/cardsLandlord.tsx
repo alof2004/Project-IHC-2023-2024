3e8ef0c280dd469399b810d19407770786bb83b9
alof04 <hpclafonso@gmail.com>
2024-05-07 18:49:24 +0100

    .

--- roomwave/src/components/cardsLandlord.tsx
+++ roomwave/src/components/cardsLandlord.tsx
@@ -54,14 +54,18 @@ const RoomSlider = () => {
   const handlePrevPage = () => {
     setActivePage((prevPage) => prevPage - 1);
   };
-  const pages = filteredRooms;
+  const pages = splitRoomsIntoPages(filteredRooms, itemsPerPage);
   if (pages.length === 0) {
-    return (<></>);
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', paddingTop:"50px" }}>
+      <button className='arrendar'>
+        <h1 style={{ fontSize: "60px" }}>Ainda não adicionou quartos na nossa plataforma</h1><h1 style={{fontSize:"60px"}}>Clique aqui para adicionar</h1></button>
+      </div>);
   }
   else
   return (
     <div className="px-0">
-      <h1 style={{ marginLeft: "4%", marginTop: "4%", fontSize: "60px" }}>Quartos vistos recentemente...</h1>
+      <h1 style={{ marginLeft: "4%", marginTop: "4%", fontSize: "60px" }}>Os meus quartos:</h1>
       <div className="room-slider-container">
         <div className="d-flex justify-content-between align-items-center">
           <button className="btn btn-light btn-xl" onClick={handlePrevPage} disabled={activePage === 0}><FiChevronLeft size={100} style={{ backgroundColor: "transparent", border: "none" }} /></button>
