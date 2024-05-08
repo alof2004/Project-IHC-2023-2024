69855c9d9ac75ff1ad4f045d9c7834dade70b6a8
alof04 <hpclafonso@gmail.com>
2024-05-08 11:43:02 +0100

    .

--- roomwave/src/components/AddRoom.tsx
+++ roomwave/src/components/AddRoom.tsx
@@ -1,4 +1,4 @@
-import NavBar from './NavBar';
+import NavBarLandLord from './NavBarLandLord';
 import RoomForm from './RoomForm';
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 function AddRoom() {
     return (
         <div>
-            <NavBar />
+            <NavBarLandLord />
             <RoomForm/>
         </div>
     );
