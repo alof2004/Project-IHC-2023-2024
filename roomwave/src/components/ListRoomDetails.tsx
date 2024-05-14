d684f9f6e8a86eedad05275c063f55b8bb97258b
alof04 <hpclafonso@gmail.com>
2024-05-14 19:49:23 +0100

    .

--- roomwave/src/components/ListRoomDetails.tsx
+++ roomwave/src/components/ListRoomDetails.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import '../css/RoomDetails2Version.css'; 
-
+import roomsData from './rooms.json'; // Adjust the path as necessary
+import '../css/RoomDetails2Version.css';
 
 interface Room {
     id: number;
@@ -42,20 +42,15 @@ interface Room {
     data_entrada: string;
     data_saida: string;
     telefone: number;
-   }
+}
 
-
-
-
-const ListRoomDetails= ({ id }: { id: number }) => {
+const ListRoomDetails = ({ id }: { id: number }) => {
     const [room, setRoom] = useState<Room | null>(null);
 
     useEffect(() => {
         const fetchRoomDetails = async () => {
             try {
-                const response = await fetch(`/rooms.json`);
-                const data: Room[] = await response.json();
-                const foundRoom = data.find(room => room.id === id);
+                const foundRoom = roomsData.find(room => room.id === id);
                 setRoom(foundRoom || null);
             } catch (error) {
                 console.error('Erro ao carregar os detalhes do quarto:', error);
@@ -64,33 +59,32 @@ const ListRoomDetails= ({ id }: { id: number }) => {
 
         fetchRoomDetails();
     }, [id]);
-  
+
     if (!room) {
-      return <p>Nenhum quarto encontrado com o ID fornecido.</p>;
+        return <p>Nenhum quarto encontrado com o ID fornecido.</p>;
     }
 
-  return (
-    <div className="room-card">
-      <h2>{room.localizacao}</h2>
-      <p>Cidade: {room.cidade}, País: {room.país}</p>
-      <p>Descrição: {room.description}</p>
-      <p>Preço: {room.price}</p>
-      <p>Mobília:</p>
-      <ul>
-        {room.mobilia.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      <p>Equipamento Disponível:</p>
-      <ul>
-        {room.Equipamento_disponivel.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      {/* Adicione outras informações conforme necessário */}
-    </div>
-  );
+    return (
+        <div className="room-card">
+            <h2>{room.localizacao}</h2>
+            <p>Cidade: {room.cidade}, País: {room.país}</p>
+            <p>Descrição: {room.description}</p>
+            <p>Preço: {room.price}</p>
+            <p>Mobília:</p>
+            <ul>
+                {room.mobilia.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+            <p>Equipamento Disponível:</p>
+            <ul>
+                {room.Equipamento_disponivel.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+            {/* Adicione outras informações conforme necessário */}
+        </div>
+    );
 };
-
 
 export default ListRoomDetails;
